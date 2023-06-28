9c6d52563b9c5d39e36fb552e2a9758e2934bb00
Eric Charnesky <eric.charnesky@careevolution.com>
2023-06-27 23:18:53 -0400

    inheritance

---
+++ Inheritance/src/TaxableItem.java
@@ -0,0 +1,27 @@
+public class TaxableItem extends Item {
+    private double taxRate;
+
+    public TaxableItem(String name, String sku, double price, int quantity, double taxRate) {
+        // the first thing you have to do in a subclass constructor,
+        // is call the super class constructor
+        super(name, sku, price, quantity);
+        setTaxRate(taxRate);
+    }
+
+    @Override
+    public double getTotalPrice(){
+        return getPrice() * getQuantity() * ( 1 + taxRate );
+    }
+
+    public double getTaxRate() {
+        return taxRate;
+    }
+
+    public void setTaxRate(double taxRate) {
+        if (taxRate < 0) {
+            this.taxRate = 0;
+        } else {
+            this.taxRate = taxRate;
+        }
+    }
+}
